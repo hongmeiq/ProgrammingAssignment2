74ff35b9687f05ca0267a395ec48135819dc2939
Hongmei Qiu <hqiu@ucsd.edu>
2014-04-24 23:56:04 -0700

    add comments to test.R

--- test.R
+++ test.R
@@ -1,5 +1,10 @@
+# a function create a matrix object
 hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# print hilbert(8)
 hilbert(8)
+# cache matrix
 x<-makeCacheMatrix(hilbert(8))
+# inverse matrix
 cacheSolve(x)
+# inverse matrix using cache
 cacheSolve(x)
